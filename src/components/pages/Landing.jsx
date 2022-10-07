7e6e0b332fa4d543073b4800a02d28f1375e54c7
Dushyant Patel <devops@bydushyant.com>
2022-10-07 11:41:42 -0400

    Created basic landing page
    - Created landing page design, requires logic
    - Planing to use context api to maintain login state.

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-const Landing = ({name}) => {
-    return (
-        <div>
-            {name} component
-        </div>
-    );
+import Card from "../common/Card";
+import './Landing.css';
+
+
+const Landing = () => {
+
+    const [login, setLogin] = useState(false);
+        return (
+            <div className='landing_page'>
+            {
+                (!login)&&    
+                <Card>
+                    <section className='login_form'>
+                        <form className='form_input' >
+                            <input className='frm_input_text' type="text" placeholder="Name" required/>
+                            <input className='frm_input_text' type="email" placeholder="Email"/>
+                            <button type='submit' className='frm_submit_btn' onClick={() => {setLogin(true)}}>Start Test</button>
+                        </form>
+                    </section>
+                </Card>
+
+            }
+            {
+                (login)&&
+                <Card>
+                    <section className='login_form'>
+                        <div>Test 1</div>
+                        <div>Test 2</div>
+                    </section>
+                </Card>
+            }
+            </div>
+        );
 };
 
 Landing.propTypes = {
-    name: PropTypes.string.isRequired
+   
 };
 
 export default Landing;
