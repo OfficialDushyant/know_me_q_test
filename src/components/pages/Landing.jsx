4bb3932bae31a413c45e3581c2dd415a6a40898e
Dushyant Patel <devops@bydushyant.com>
2022-10-08 12:34:12 -0400

    Login form validation
    - Validate name and email before Starting test

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { useFormik } from 'formik';
+import * as Yup from "yup";
 
 import Card from "../common/Card";
 import RadioButton from "../common/RadioButton";
@@ -11,17 +12,47 @@ import './Landing.css';
 
 const Landing = () => {
 
-    const { login, setLogIn } = useTest();
+    const { login, setLogIn, setTest } = useTest();
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .required("Please enter your email address")
+                .email("Please enter valid email address"),
+            name: Yup.string()
+                .required("Please enter your name"),
+
+        }),
+        onSubmit: async (values) => {
+            try {
+                const response = await fetch(BASE_URL + "/Tests.json")
+                if(!response.ok) {
+                    throw new Error("API Failed");
+                }
+                const tests = await response.json();
+                setTest(tests);
+                setLogIn(true);
+            } catch(error) {
+                console.log(error);
+            }
+        }
+    })
         return (
             <div className='landing_page'>
             {
                 (!login)&&    
                 <Card>
                     <section className='login_form'>
-                        <form className='form_input' >
-                            <input className='frm_input_text' type="text" placeholder="Name" required/>
-                            <input className='frm_input_text' type="email" placeholder="Email"/>
-                            <button type='submit' className='frm_submit_btn' onClick={() => {setLogin(true)}}>Start Test</button>
+                        <form onSubmit={formik.handleSubmit} className='form_input'>
+                                <input className="frm_input_text" type="text" placeholder="Name" name="name" onChange={formik.handleChange}/>
+                                { formik.touched.name && formik.errors.name && <span>{formik.errors.name}</span> }
+                                <input className='frm_input_text' type="email" placeholder="Email" name="email" onChange={formik.handleChange} />
+                                { formik.touched.email && formik.errors.email && <span>{formik.errors.email}</span> }
+                            <RadioButton label={"Select"}/>
+                            <button type='submit' className='frm_submit_btn'>Start Test</button>
                         </form>
                     </section>
                 </Card>
